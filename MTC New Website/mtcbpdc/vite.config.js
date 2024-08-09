807d0d118575ab7c0bec9633fe9eaf4ad73d9808
Stellin-15 <123811154+Stellin-15@users.noreply.github.com>
2024-08-10 01:19:32 +0400

    changes made
    this time fixed the mobile view for the about us page where the navbar and the image was overlapping and also the homepage being loaded first

--- MTC New Website/mtcbpdc/vite.config.js
+++ MTC New Website/mtcbpdc/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mtcbpdc_stellin_s_version/',  // Replace <REPO_NAME> with your repository name
+    // Replace <REPO_NAME> with your repository name
 })
 
