70e9fe7f664aea40eb650930369f0fc2beff5c19
Stellin-15 <123811154+Stellin-15@users.noreply.github.com>
2024-08-09 20:15:56 +0400

    delete
    sorry

--- MTC New Website/mtcbpdc/src/pages/Events/Events.jsx
+++
@@ -1,9 +0,0 @@
-// src/pages/Events/Events.jsx
-import React from 'react';
-import './Events.css'; // Import the CSS file
-
-const Events = () => {
-  return <div className="events">Events Page</div>;
-};
-
-export default Events;
