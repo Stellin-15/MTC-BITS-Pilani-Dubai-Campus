c6d362808c37fbc642500767fcb4bc5c129f4a35
Stellin-15 <123811154+Stellin-15@users.noreply.github.com>
2024-08-09 20:16:56 +0400

    third commit
    test 1 final

---
+++ MTC New Website/mtcbpdc/src/components/Footer/Footer.jsx
@@ -0,0 +1,40 @@
+// src/components/Footer/Footer.jsx
+import React from 'react';
+import './Footer.css';
+import { FaTwitter, FaInstagram, FaLinkedin, FaDiscord, FaTiktok } from 'react-icons/fa';
+import { RiThreadsFill } from 'react-icons/ri';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-icons">
+        <a href=" https://x.com/mtcbpdc" target="_blank" rel="noopener noreferrer">
+          <FaTwitter className="footer-icon" />
+        </a>
+        <a href=" https://instagram.com/mtcbpdc" target="_blank" rel="noopener noreferrer">
+          <FaInstagram className="footer-icon" />
+        </a>
+        <a href=" https://discord.gg/VDtpu9kF" target="_blank" rel="noopener noreferrer">
+          <FaDiscord className="footer-icon" />
+        </a>
+        <a href=" https://www.threads.net/@mtcbpdc" target="_blank" rel="noopener noreferrer">
+          <RiThreadsFill className="footer-icon" />
+        </a>
+        <a href="https://www.linkedin.com/company/microsoft-tech-club/mycompany/" target="_blank" rel="noopener noreferrer">
+          <FaLinkedin className="footer-icon" />
+        </a>
+        <a href=" https://tiktok.com/@mtcbpdc" target="_blank" rel="noopener noreferrer">
+          <FaTiktok className="footer-icon" />
+        </a>
+      </div>
+      <div className="footer-contact">
+        <a href="mailto:microsofttechclub@dubai.bits-pilani.ac.in">microsofttechclub@dubai.bits-pilani.ac.in</a>
+      </div>
+      <div className="footer-copy">
+        &copy; 2024 Microsoft Tech Club, BITS Pilani Dubai Campus
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ MTC New Website/mtcbpdc/src/pages/Events/Events.jsx
@@ -0,0 +1,9 @@
+// src/pages/Events/Events.jsx
+import React from 'react';
+import './Events.css'; // Import the CSS file
+
+const Events = () => {
+  return <div className="events">Events Page</div>;
+};
+
+export default Events;
