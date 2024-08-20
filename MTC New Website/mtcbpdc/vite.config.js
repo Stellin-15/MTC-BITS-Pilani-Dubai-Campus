c063dbb78f8b6ebde308274c39ddb9ece7e6fd61
Stellin-15 <123811154+Stellin-15@users.noreply.github.com>
2024-08-20 15:58:36 +0400

    changes
    for deploy test

--- MTC New Website/mtcbpdc/vite.config.js
+++ MTC New Website/mtcbpdc/vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-    // Replace <REPO_NAME> with your repository name
+  base:"/mtcbpdc_stellin_s_version/"
 })
 
