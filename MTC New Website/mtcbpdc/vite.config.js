abaff7cd66c63a9a86c237d05f744646f79f082d
Stellin-15 <123811154+Stellin-15@users.noreply.github.com>
2024-08-28 00:12:11 +0400

    changes to repo name

--- MTC New Website/mtcbpdc/vite.config.js
+++ MTC New Website/mtcbpdc/vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mtcbpdc_stellin_s_version/',
+  base: '/MTC-BITS-Pilani-Dubai-Campus/',
 })
 
