708ab14a5ed283af594de3b3d628b7ea3872c30d
Stellin-15 <123811154+Stellin-15@users.noreply.github.com>
2024-08-27 23:22:47 +0400

    changes made to events page

--- MTC New Website/mtcbpdc/src/pages/Events/Events.jsx
+++ MTC New Website/mtcbpdc/src/pages/Events/Events.jsx
@@ -19,7 +19,7 @@ const EventCard = () => {
           <p className="event-title">VS CODE WORKSHOP</p>
           <p className="event-time">6:30 PM - 7:45 PM (ONLINE)</p>
           <p className="event-date">20th November, 2023</p>
-          <a href="https://www.instagram.com/p/Czv9N40NEYV/">
+          <a href="">
             <div className="event-button">Register Now</div>
           </a>
         </div>
@@ -40,7 +40,7 @@ const EventCard = () => {
           <p className="event-description">
             The "How To Start Programming" workshop began with the MTC core council outlining the club's goals and upcoming events. The speakers then covered programming basics, highlighted BITS Pilani's offerings, and provided an introduction to competitive programming and open-source projects. Participants also received a curated list of resources to start learning their preferred languages.
           </p>
-          <a href="https://www.instagram.com/p/Czv9N40NEYV/">
+          <a href="https://www.instagram.com/reel/CwxdLidvP2h/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==">
             <div className="event-button blue-button">Read More</div>
           </a>
         </div>
@@ -58,7 +58,7 @@ const EventCard = () => {
           <p className="event-description">
             The “MATLAB Workshop” began with the two speakers introducing the UI of MATLAB and its basic syntax and functionalities. They moved on to talking about the data types, comments, arrays, linspace, matrix element-wise operations, and indexing. Next, they solved two sample problems that aimed to demonstrate problem-solving and plotting graphs in MATLAB.
           </p>
-          <a href="https://www.instagram.com/p/Czv9N40NEYV/">
+          <a href="https://www.instagram.com/p/CxDTqwDv-l0/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==">
             <div className="event-button blue-button">Read More</div>
           </a>
         </div>
@@ -94,7 +94,7 @@ const EventCard = () => {
           <p className="event-description">
             The Typing Contest, Microsoft Tech Club's flagship event, tested participants' auditory comprehension and typing speed in a Computer Lab. Participants had two hours to attempt the challenge, with the opportunity for two retries. Each hour featured four unique audio clips, and participants were evaluated on speed and accuracy. The competition had a total of 9 winners, with 3 winners from each hour.
           </p>
-          <a href="https://www.instagram.com/p/Czv9N40NEYV/">
+          <a href="https://www.instagram.com/p/Cxa9BRFvVWh/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==">
             <div className="event-button blue-button">Read More</div>
           </a>
         </div>
@@ -112,7 +112,7 @@ const EventCard = () => {
           <p className="event-description">
             The MTC SignQuest is a promotional event for the ESL Global Summit Generative AI Competition, organized by MTC during the CINS 2023 conference at BITS Pilani Dubai Campus in hybrid mode. Held in the vending machine area, pairs of participants took part, with one answering quiz questions while the other used search skills to assist. The competition aimed to raise awareness of Emirati Sign Language and the Generative AI competition.
           </p>
-          <a href="https://www.instagram.com/p/Czv9N40NEYV/">
+          <a href="https://www.instagram.com/p/CyQ5r_ivx8Q/?utm_source=ig_web_copy_link">
             <div className="event-button blue-button">Read More</div>
           </a>
         </div>
@@ -130,7 +130,7 @@ const EventCard = () => {
           <p className="event-description">
             ThinkAI'23 sought to find students with AI-driven solutions to global challenges, providing mentorship for the 2024 Microsoft Imagine Cup. Fourteen out of 16 registered teams presented ideas in areas like sustainability, healthcare, and education to 8 judges. The winners were Team Tech Titans (Sanober Sarfaraz Ahmed and Harsh Garg) and Team IntelliTrack (Anurag Kumar Jha, Mohammed Husamuddin, Sstuti D Mehra, Abdul Rahmaan Ansari).
           </p>
-          <a href="https://www.instagram.com/p/Czv9N40NEYV/">
+          <a href="https://www.linkedin.com/posts/microsoft-tech-club_%F0%9D%97%A0%F0%9D%97%B6%F0%9D%97%B0%F0%9D%97%BF%F0%9D%97%BC%F0%9D%98%80%F0%9D%97%BC%F0%9D%97%B3%F0%9D%98%81-%F0%9D%97%A7%F0%9D%97%B2%F0%9D%97%B0%F0%9D%97%B5-%F0%9D%97%96%F0%9D%97%B9%F0%9D%98%82%F0%9D%97%AF%F0%9D%98%80-%F0%9D%97%A7%F0%9D%97%B5%F0%9D%97%B6%F0%9D%97%BB%F0%9D%97%B8%F0%9D%97%94%F0%9D%97%9C-activity-7140743739425153024-at_b?utm_source=share&utm_medium=member_desktop">
             <div className="event-button blue-button">Read More</div>
           </a>
         </div>
@@ -148,7 +148,7 @@ const EventCard = () => {
           <p className="event-description">
             The Microsoft Tech Club hosted a speaker session with Professor Nick Pears, a Computer Vision expert from the University of York. Attended by BITS Pilani Dubai Campus director Dr. Srinivasan Madapusi and Computer Science HoD Dr. Vijaykumar, the event covered AI, Cyber Security, and Professor Pears' work in craniofacial shape modeling, sparking discussions on the future of Computer Vision.
           </p>
-          <a href="https://www.instagram.com/p/Czv9N40NEYV/">
+          <a href="https://www.linkedin.com/posts/microsoft-tech-club_microsofttechclub-computervision-techinnovation-activity-7143636619009466368-QdG9?utm_source=share&utm_medium=member_desktop">
             <div className="event-button blue-button">Read More</div>
           </a>
         </div>
